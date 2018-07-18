137479f1276e613056b08871b80fd20b6d638ed5
Paul Ciarlo <paul.ciarlo@gmail.com>
2018-07-18 12:06:35 -0400

    imported and modified http3 project from boost

---
+++ src/client.cpp
@@ -0,0 +1,13 @@
+//
+//  client.cpp
+//  Magick
+//
+//  Created by Paul Ciarlo on 7/17/18.
+//  Copyright © 2018 Paul Ciarlo. All rights reserved.
+//
+
+#include "client.hpp"
+
+namespace server4 {
+    
+}
---
+++ src/client.hpp
@@ -0,0 +1,17 @@
+//
+//  client.hpp
+//  Magick
+//
+//  Created by Paul Ciarlo on 7/17/18.
+//  Copyright © 2018 Paul Ciarlo. All rights reserved.
+//
+
+#ifndef client_hpp
+#define client_hpp
+
+#include <stdio.h>
+
+namespace server4 {
+    
+}
+#endif /* client_hpp */
